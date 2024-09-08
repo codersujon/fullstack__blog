8c0a54264e551c593d6b42cc3840e129f32fab50
unknown <csesujon155@gmail.com>
2024-09-08 16:00:24 -0700

    Route Any method use

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Any Slug return welcome blade
+Route::any('{slug}', function(){
+    return view('welcome');
+});
